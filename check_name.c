cdf04423fca7d618eae73c2754914f7495b64a93
Samantha Delgado <sss.delgad@gmail.com>
2023-02-23 22:21:25 -0700

    New file - check dictionary

---
+++ check_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Implement an autocomplete system. 
+// That is, given a query string s and a set of all possible query strings, return all strings in the set that have s as a prefix.
+
+// For example, given the query string de and the set of strings [dog, deer, deal], return [deer, deal].
+
+int ret_autocomplete(char* str_of_str, char* key_to_fill, int len_of_strstr, int len_of_key){
+    // dependent on the length of the key
+    
+}
+
+int main(){
+    char str_arr
+}
